f31e79fdadadd65ee472bfa1fb452b9c4473856e
MIke C <conawaymike1985@gmail.com>
2025-02-13 11:26:39 -0800

    Improved damage overlays. Follows world location as overlay position updates.

--- Assets/1 - Scripts/UI/UIDamageNumbers.cs
+++ Assets/1 - Scripts/UI/UIDamageNumbers.cs
@@ -6,40 +6,28 @@ public class UIDamageNumbers : Singleton<UIDamageNumbers>
 {
 
     public UIDamagePop DamagePrefab;
-    private RectTransform _rectTransform;
 
 
     protected override void Awake()
     {
         base.Awake();
-        _rectTransform = transform as RectTransform;
     }
 
 
-    public void CreatePop( StatisticChange statChange, Vector3 worldPos )
+    public void CreatePop( StatisticChange statChange, Vector3 worldPosTarget )
     {
         if( DamagePrefab == null )
             return;
-
         var pop = Instantiate<UIDamagePop>( DamagePrefab );
-        pop.transform.SetParent( this.transform, false );
-        pop.Initialize( statChange.GetChangeStringified() );
-        var rt = pop.transform as RectTransform;
-        rt.SetCanvasScreenPosition( _rectTransform, worldPos + Random.insideUnitSphere * .1f );
-        pop.DoRise();
+        pop.Initialize( this.transform, statChange.GetChangeStringified(), worldPosTarget + Random.insideUnitSphere * .1f );
     }
 
-    public void CreatePop( string message, Vector3 worldPos )
+    public void CreatePop( string message, Vector3 worldPosTarget )
     {
         if( DamagePrefab == null )
             return;
-
         var pop = Instantiate<UIDamagePop>( DamagePrefab );
-        pop.transform.SetParent( this.transform, false );
-        pop.Initialize( message );
-        var rt = pop.transform as RectTransform;
-        rt.SetCanvasScreenPosition( _rectTransform, worldPos + Random.insideUnitSphere * .1f );
-        pop.DoRise();
+        pop.Initialize( this.transform, message, worldPosTarget );
     }
 
 }
--- Assets/1 - Scripts/UI/UIDamagePop.cs
+++ Assets/1 - Scripts/UI/UIDamagePop.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
 
 public class UIDamagePop : MonoBehaviour
 {
@@ -16,7 +17,9 @@ public class UIDamagePop : MonoBehaviour
 
     private CanvasGroup _canvasGroup;
     private RectTransform _rectTransform;
-    private Vector2 _riseStart;
+    private RectTransform _container;
+    private Vector3 _offset = Vector2.zero;
+    private Vector3 _worldPosTarget;
 
 
     private void Awake()
@@ -26,11 +29,17 @@ public class UIDamagePop : MonoBehaviour
     }
 
 
-    public void Initialize( string number )
+    public void Initialize( Transform container, string number, Vector3 worldPosTarget )
     {
+        transform.SetParent( container, false );
         if( this.Value == null )
             return;
+        _worldPosTarget = worldPosTarget;
         this.Value.text = number;
+        _container = container as RectTransform;
+
+        UpdatePosition();
+        DoRise();
     }
 
 
@@ -39,14 +48,27 @@ public class UIDamagePop : MonoBehaviour
     /// </summary>
     public void DoRise()
     {
-        _riseStart = _rectTransform.anchoredPosition;
+        _offset = Vector3.zero;
         CoroutineUtils.DoInterpolation( RiseDuration, f =>
         {
-            _rectTransform.anchoredPosition = _riseStart + Vector2.up * f * RiseDistance;
+            _offset = Vector3.up * f * RiseDistance;
             _canvasGroup.alpha = FadeCurve.Evaluate( f );
             return true;
         },
         () => { Destroy( gameObject ); }
         );
+    }
+
+    private void Update()
+    {
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
+        if( _container == null )
+            return;
+        if( _rectTransform != null )
+            _rectTransform.SetCanvasScreenPosition( _container, _worldPosTarget + _offset );
     }
 }
